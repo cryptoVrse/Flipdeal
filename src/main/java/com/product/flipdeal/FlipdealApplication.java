2a8a4e648503073dee6fac3ab4222837a70c9f13
Abhishek2013 <abhishek.agrawal@capillarytech.com>
2023-11-03 18:56:30 +0530

    created spring boot project

---
+++ src/main/java/com/product/flipdeal/FlipdealApplication.java
@@ -0,0 +1,13 @@
+package com.product.flipdeal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlipdealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlipdealApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/product/flipdeal/FlipdealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.product.flipdeal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlipdealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
